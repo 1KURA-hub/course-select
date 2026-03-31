14b8464e5d3412429d4022cb580ca4823993c0c0
1KURA <1823748901@qq.com>
2026-03-31 20:09:12 +0800

    perf(course): 移除当前读再校验库存逻辑，采用单条SQL原子更新重构库存扣减

--- service/createrecord.go
+++ service/createrecord.go
@@ -9,7 +9,6 @@ import (
 
 	"go.uber.org/zap"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 // 创建选课记录
@@ -19,51 +18,43 @@ func CreateRecord(timeoutCtx context.Context, studentID, courseID uint) error {
 		var course model.Course
 		course.ID = courseID
 
-		// select for update 对库存上记录锁
-		err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where("id = ?", course.ID).First(&course).Error
+		db := tx.Model(&model.Course{}).
+			Where("id = ? AND stock > ?", courseID, 0).
+			Update("stock", gorm.Expr("stock-1"))
+
+		if db.Error != nil {
+			global.Logger.Error(ErrStockEmpty.Error(), zap.Error(db.Error))
+			return errors.New("MySQL课程库存更新失败")
+		}
+
+		// 影响行数为0 更新失败
+		if db.RowsAffected == 0 {
+			global.Logger.Debug(ErrStockEmpty.Error())
+			return ErrStockEmpty
+		}
+
+		// 创建选课记录实例
+		var selection = &model.Selection{
+			StudentID: studentID,
+			CourseID:  course.ID,
+		}
+
+		// 选课记录写入数据库
+		err := tx.Create(selection).Error
 		if err != nil {
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				global.Logger.Debug("课程不存在", zap.Uint("course_id", courseID))
-				return errors.New("课程不存在")
+			// 判断是否是唯一索引冲突
+			if strings.Contains(err.Error(), "Duplicate entry") {
+				global.Logger.Error("重复选课", zap.Error(err))
+				return ErrRepeatSelection
 			}
-			global.Logger.Error("数据库查询课程时出错", zap.Uint("course_id", courseID), zap.Error(err))
-			return ErrSystemBusy
+
+			global.Logger.Error("数据库新建选课记录出错",
+				zap.Uint("studentID", studentID),
+				zap.Uint("course_id", courseID),
+				zap.Error(err))
+			return err
 		}
-
-		if course.Stock > 0 {
-			// 更新课程库存
-			course.Stock--
-			err = tx.Model(&course).Update("stock", course.Stock).Error
-			if err != nil {
-				global.Logger.Error("数据库更新课程库存时出错", zap.Uint("course_id", courseID), zap.Error(err))
-				return ErrSystemBusy
-			}
-
-			// 创建选课记录实例
-			var selection = &model.Selection{
-				StudentID: studentID,
-				CourseID:  course.ID,
-			}
-
-			// 选课记录写入数据库
-			err = tx.Create(selection).Error
-			if err != nil {
-				// 判断是否是唯一索引冲突
-				if strings.Contains(err.Error(), "Duplicate entry") {
-					global.Logger.Error("重复选课", zap.Error(err))
-					return ErrRepeatSelection
-				}
-
-				global.Logger.Error("数据库新建选课记录出错",
-					zap.Uint("studentID", studentID),
-					zap.Uint("course_id", courseID),
-					zap.Error(err))
-				return err
-			}
-			return nil
-		}
-		global.Logger.Debug("课程库存不足", zap.Uint("course_id", courseID))
-		return ErrStockEmpty
+		return nil
 	})
 
 }
