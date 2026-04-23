6f5f3a412b6dd52d3e7d92a55632ffe64857b0dd
Huayang Gao <g1823748901@gmail.com>
2026-04-23 14:13:14 +0800

    docs: 补充库存有限压测脚本

---
+++ scripts/post.lua
@@ -0,0 +1,30 @@
+local tokens = {}
+local file = io.open("tokens_10000.txt", "r")
+
+if not file then
+    error("failed to open token file")
+end
+
+for line in file:lines() do
+    if line ~= "" then
+        table.insert(tokens, line)
+    end
+end
+file:close()
+
+local idx = 1
+
+request = function()
+    local token = tokens[idx]
+    idx = idx + 1
+    if idx > #tokens then
+        idx = 1
+    end
+
+    local headers = {
+        ["Authorization"] = "Bearer " .. token,
+        ["Content-Type"] = "application/json"
+    }
+
+    return wrk.format("POST", "/auth/select/1", headers, nil)
+end
